e4a0238db63d6be4dd2d5733e28a52f85b7f35bc
Bruno Silva <brs.rsilva@gmail.com>
2021-05-30 17:53:16 -0300

    basic struture and first component (ativos)

---
+++ src/components/App/App.tsx
@@ -0,0 +1,77 @@
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  CalculatorOutlined,
+  UserOutlined,
+  HomeOutlined,
+  BankOutlined,
+} from '@ant-design/icons';
+import { useState } from 'react';
+
+import '../../style/global.scss';
+import Body from '../Body/Body';
+import Ativos from '../Ativos';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+export default function App(){
+const [collapsed, setCollapsed] = useState(false);
+
+const [selectedComponent, setSelectedComponent] = useState("");
+  
+const state = {
+  selectedComponent,
+  setSelectedComponent
+}
+
+function handleClickButton(component: string) {
+  setSelectedComponent(component);
+}
+
+
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item 
+              key="1" 
+              onClick={() => handleClickButton("ativos")}
+              icon={<CalculatorOutlined />}>
+              Ativos
+            </Menu.Item>
+            <Menu.Item 
+              key="2" 
+              onClick={() => handleClickButton("usuarios")}
+              icon={<UserOutlined />}>
+              Usuários
+            </Menu.Item>
+            <Menu.Item 
+              key="3" 
+              onClick={() => handleClickButton("unidades")}
+              icon={<HomeOutlined />}>
+              Unidades
+            </Menu.Item>
+            <Menu.Item 
+              key="4" 
+              onClick={() => handleClickButton("empresas")}
+              icon={<BankOutlined />}>
+              Empresas
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              <Ativos />
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+  )
+}
---
+++ src/components/Ativos/index.tsx
@@ -0,0 +1,111 @@
+import { Typography } from 'antd';
+import { Table, Tag, Space, Avatar } from 'antd';
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+
+import style from './style.module.scss';
+
+const { Title } = Typography;
+
+interface AtivosProps {
+    id: number;
+    sensors: Array<{
+        name: string
+    }>;
+    model: string;
+    status: string;
+    healthscore: number;
+    name: string;
+    image: string;
+    specifications: Array<{
+        maxTemp: number;
+        power: number;
+        rpm: number;
+    }>;
+    metrics: Array<{
+        totalCollectsUptime: number;
+        totalUptime: number;
+        lastUptimeAt: Date;
+    }>;
+    unitId: number;
+    companyId: number;
+}
+
+
+export default function Ativos() {
+    const [ativos, setAtivos] = useState<AtivosProps[]>([]);
+
+    useEffect(() => {
+        api.get<AtivosProps[]>(`/assets`).then(response => {
+            setAtivos(response.data);
+        })
+    }, []);
+
+    const data = ativos.map(row => ({
+        id: row.id,
+        sensors: row.sensors,
+        productimage: row.image,
+        name: row.name,
+        model: row.model,
+        status: row.status,
+        healthscore: row.healthscore
+    }))
+
+    const columns = [
+        {
+            title: 'Id',
+            dataIndex: 'id',
+            key: 'id',
+            className: 'idColumn',
+        },
+        {
+            title: 'Sensor',
+            dataIndex: 'sensors',
+            key: 'sensors',
+            className: 'sensorColumn',
+        },
+        {
+            title: '',
+            dataIndex: 'image',
+            key: 'image',
+            render: (text: any, record: { productimage: string | undefined; }) => {
+                return (
+                    <img src={record.productimage} />
+                );
+            },
+            className: 'imageColumn',
+        },
+        {
+            title: 'Nome',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Model',
+            dataIndex: 'model',
+            key: 'model',
+        },
+        {
+            title: 'Status',
+            dataIndex: 'status',
+            key: 'status',
+            className: 'statusColumn',
+        },
+        {
+            title: 'Healthscore',
+            key: 'healthscore',
+            dataIndex: 'healthscore',
+            className: 'healthColumn',
+        }
+    ];
+
+
+
+    return (
+        <div className={style.container}>
+            <Title level={3}>Ativos</Title>
+
+            <Table columns={columns} dataSource={data} />
+        </div>
+    )
+}
