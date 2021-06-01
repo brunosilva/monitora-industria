f844b64343e9f0ba5ba3d6ddf851480b76e1fb77
Bruno Silva <brs.rsilva@gmail.com>
2021-06-01 14:47:37 -0300

    populate chart - (error)

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from 'react';
+import Highcharts from 'highcharts';
+import {
+    HighchartsChart, Chart, withHighcharts, XAxis, YAxis, Title, Legend, ColumnSeries, SplineSeries, PieSeries
+} from 'react-jsx-highcharts';
 
 import { api } from '../../services/api';
+import HighchartsReact from 'highcharts-react-official';
 
 interface UserUnit {
     unitId: number;
 }
 
-export default function Body(){
+export default function Body() {
     const [usersUnit, setUsersUnit] = useState<UserUnit[]>([]);
 
     useEffect(() => {
@@ -14,13 +19,61 @@ export default function Body(){
             setUsersUnit(response.data);
         })
     });
-    const result = usersUnit.reduce(function (acumulador, objetoAtual){
-        return acumulador + objetoAtual.unitId;
-      }, 0);
 
-    return(
+    const unidade1 = usersUnit.filter((e) => e.unitId === 1).length;
+    const unidade2 = usersUnit.filter((e) => e.unitId === 2).length;
+
+
+    const optionsChart = {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Total Usuários por unidade'
+        },
+        xAxis: {
+            crosshair: true
+        },
+        yAxis: {
+            title: {
+                text: 'Quantidade (un)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y} </b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [
+            {
+                name: 'Unidade Jaguar',
+                data: [4]
+
+            },
+            {
+                name: 'Unidade Tobias',
+                data: [2]
+
+            }
+        ]
+    }
+
+
+    return (
         <>
-            {result}
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={optionsChart}
+            />
         </>
     )
 }
